fe0a37bc10550f8d14f9a00eac5d827696f34a87
Joseph Cybul zebede <josephcybulzebede@Josephs-MacBook-Air.local>
2026-06-05 14:00:25 -0500

    initial project setup

---
+++ ReelWorldTests/ReelWorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReelWorldTests.swift
+//  ReelWorldTests
+//
+//  Created by Joseph Cybul zebede  on 6/5/26.
+//
+
+import Testing
+@testable import ReelWorld
+
+struct ReelWorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
